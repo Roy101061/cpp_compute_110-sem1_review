129853115a9d29b7bc5272ec9634bdb491a81f56
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2022-01-10 23:16:13 +0800

    Update For.cpp

--- loops/For/For.cpp
+++ loops/For/For.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // "For" ex:
 
 #include <iostream>
@@ -12,18 +11,3 @@ int main(){
         cout<<b<<endl;
     }
 }
-=======
-// "For" ex:
-
-#include <iostream>
-
-using namespace std;
-
-int main(){
-    int a;
-    cin>>a;
-    for(int b=0;b<=a;b++){
-        cout<<b<<endl;
-    }
-}
->>>>>>> 18a76d77d3f66de0081688b6ce69f9151d029a0b
