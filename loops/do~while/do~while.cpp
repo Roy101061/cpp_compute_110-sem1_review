9bfc30455c5a89cf20873dc6deee89fd226cb28a
Roy101061 <Roy101061@gmail.com>
2022-01-10 15:24:05 +0000

    Update

---
+++ loops/do~while/do~while.cpp
@@ -0,0 +1,15 @@
+// "do~while" ex:
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    int b=0;
+    cin>>a;
+    do{
+        cout<<b<<endl;
+        b++;
+    }while(b<=a);
+}
