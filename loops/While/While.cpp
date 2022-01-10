18a76d77d3f66de0081688b6ce69f9151d029a0b
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2022-01-10 23:12:17 +0800

    Create While.cpp

---
+++ loops/While/While.cpp
@@ -0,0 +1,15 @@
+// "While" ex:
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    int b=0;
+    cin>>a;
+    while(b<=a){
+        cout<<b<<endl;
+        b++;
+    }
+}
